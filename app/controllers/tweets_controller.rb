0eaa4ce8b951e164e5c2c852f9ad2260f7cada7e
Alex <abrodersen@student.uaustin.org>
2025-02-15 16:44:12 -0600

    We made the form field save tweets and then refresh the field so people can add more tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,17 @@ def new
 	@tweet = Tweet.new
 end
 
+def create
+	print "rex rex fire fire "
+	print params["tweet"]["content"]
+	print "cold cold cold cold cold "
+	@tweet = Tweet.new
+	@tweet.content = params["tweet"]["content"]
+	@tweet.save
+
+	redirect_to new_tweet_path
+	# this is the code that will create a new tweet
+	# and then go back to the beginning with a fresh form
+end	
+
 end
